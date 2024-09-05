ebe33c501115004142839ee8dcbf4576bced8828
shambhu <shambhusingh1309@gmail.com>
2024-09-05 16:15:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://ums.lpu.in/lpuums/assets/login/img/logos/seal.svg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ums.lpu.in/lpuums/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Shambhu Saran Singh welcome to lpu
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+ let name = "shambhu";
 root.render(
   <React.StrictMode>
-    <App />
+    
+    {/* <App /> */}
+    {/* <>
+      <h1>React is {5+5} times better with JSX</h1>
+      <p>It is a paragraph</p>
+    </> */}
+    {/* <div>
+      <h1>React is {5+5} times better with JSX</h1>
+      <p>It is a paragraph</p>
+    </div> */}
+
+    <>
+    <h1>Hello {name}</h1>
+    <p>you are in LPU</p>
+    </>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// reportWebVitals();
